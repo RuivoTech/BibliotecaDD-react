035ee1bc0fab5b9009af144ab6b55cacde7dcc1d
Richieri Negri <ruivotech@hotmail.com>
2020-06-16 17:33:21 -0300

    Adicionado script de build e versão do sistema.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import package from "../../../package.json";
 
 const Footer = () => {
     return (
         <footer className="fixed-bottom bg-white" style={{ height: "5vh" }}>
-            <p className="h6 text-center">Todos os direitos reservados <a target="_blank" href="https://github.com/RuivoTech">RuivoTech</a></p>
+            <p className="h6 text-center">
+                Todos os direitos reservados {" "}
+                <a target="_blank" href="https://github.com/RuivoTech">
+                    RuivoTech
+                </a>
+                <span className="h6 text-secondary">Versão {package.version}</span>
+            </p>
 
         </footer>
     )
