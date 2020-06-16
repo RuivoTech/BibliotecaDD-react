b2a45d227cd847b850604acff4f20c46349dc268
Richieri Negri <ruivotech@hotmail.com>
2020-06-16 17:17:38 -0300

    Updates

---
+++ precache-manifest.bfaab7ad155b506b66e4127ec2e9d7f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714f630bd9111de43d7d3bfe7fe1a0e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00802ae53ebe83b2c2b1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c67b9125be5cbc352273",
+    "url": "/static/css/main.12738e05.chunk.css"
+  },
+  {
+    "revision": "00802ae53ebe83b2c2b1",
+    "url": "/static/js/2.2aa56644.chunk.js"
+  },
+  {
+    "revision": "54290d94de474a204cee26e039f2fb48",
+    "url": "/static/js/2.2aa56644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67b9125be5cbc352273",
+    "url": "/static/js/main.230ff5f7.chunk.js"
+  },
+  {
+    "revision": "ef71f4fd5d36b840f037",
+    "url": "/static/js/runtime-main.61f6b37a.js"
+  },
+  {
+    "revision": "ed330dc2604ff21d30662085c5f49bbb",
+    "url": "/static/media/Logo.ed330dc2.png"
+  }
+]);
