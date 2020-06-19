9a200a7394c88aadafc3ed3dacd48c400fb93e9c
Richieri Negri <ruivotech@hotmail.com>
2020-06-19 17:18:46 -0300

    Centralizando tela de login.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -70,7 +70,7 @@ const Login = () => {
 
     return (
         <>
-            <div className="auth-wrapper">
+            <div className="auth-wrapper align-items-center">
                 <div className="auth-inner">
                     <form onSubmit={handleLogin}>
                         <h3>Sign In</h3>
